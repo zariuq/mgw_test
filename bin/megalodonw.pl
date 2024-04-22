4e3ddbe06c3b1b381af5be86cb9278982c1966c3
Anonymous <>
2024-04-22 15:59:05 +0200

    theory

--- bin/megalodonw.pl
+++ bin/megalodonw.pl
@@ -6,6 +6,7 @@ my $mgbinary = "./bin/megalodon"; # location of megalodon
 my $tl = 1 + shift @ARGV; # the first argument is the time limit
 my $mgfile = shift @ARGV; # the second argument is the .mg file to be checked (it may be repeated later in the regular megalodon command)
 my $mginclude = ""; # the includes we will collect from $mgfile - can be empty or contain multiple -I .mgs
+my $theory =""; # the optional theory - hf, hoas, mizar - only one is allowed; if more $T lines are given, only the last one is used
 
 open(F,$mgfile) or die "File not readable: $mgfile";
 
@@ -13,6 +14,7 @@ open(F,$mgfile) or die "File not readable: $mgfile";
 while(<F>)
   {
     if(m/^\s*\(\*\*\s+\$I\s+(\S+)\s+\*\*\)\s*/) { $mginclude = $mginclude . "-I $1 ";}
+    if(m/^\s*\(\*\*\s+\$T\s+(\S+)\s+\*\*\)\s*/) { $theory = "-$1 ";}
   }
 
-exec("timeout -k1 $tl  $mgbinary $mginclude @ARGV ");
+exec("timeout -k1 $tl  $mgbinary $theory $mginclude @ARGV ");
