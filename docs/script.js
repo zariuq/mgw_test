0bb8f7292a1b301a0369b1909e07e6c47dfa75ac
Anonymous <>
2024-04-23 00:35:36 +0200

    index

---
+++ docs/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  const username = "mgwiki";
+  const repo = "mgw_test";
+  const apiUrl = `https://api.github.com/repos/${username}/${repo}/contents`;
+
+  $.ajax({
+    url: apiUrl,
+    dataType: "json",
+    success: function (data) {
+      data.forEach(function (item) {
+        let listItem = $("<li></li>");
+        let itemLink = $("<a></a>");
+
+        if (item.type === "file") {
+          itemLink.attr("href", item.download_url);
+          itemLink.text(item.name);
+        } else if (item.type === "dir") {
+          itemLink.attr("href", item.html_url);
+          itemLink.text(item.name + "/");
+        }
+
+        listItem.append(itemLink);
+        $("#directory-list").append(listItem);
+      });
+    },
+    error: function (error) {
+      console.error("Error fetching directory contents:", error);
+    },
+  });
+});
